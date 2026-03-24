588b39d64ec003415d3b897772e8c4ad97cb0534
lowen <lowen@students.uni-mainz.de>
2026-03-24 11:42:42 +0100

    fixed type error in create_board and fill_board method

--- shared/state/game_state.py
+++ shared/state/game_state.py
@@ -23,6 +23,31 @@ class TreasureType(Enum):
     RED = 2
     GREEN = 3
 
+    SKULL = 4
+    SWORD = 5
+    GOLDBAG = 6
+    KEYS = 7
+    EMERALD = 8
+    ARMOR = 9
+    BOOK = 10
+    CROWN = 11
+    CHEST = 12
+    CANDLE = 13
+    MAP = 14
+    RING = 15
+    DRAGON = 16
+    GHOST = 17
+    BAT = 18
+    GOBLIN = 19
+    PRINCESS = 20
+    GENIE =  21
+    BUG = 22
+    OWL = 23
+    LIZARD = 24
+    SPIDER = 25
+    FLY =  26
+    RAT =  27
+
 class Tile:
     def __init__(self, type: TileType, orientation: TileOrientation, treasure: None | TreasureType = None):
         self.type = type
@@ -31,53 +56,56 @@ class Tile:
 
 class Board:
     def __init__(self, width: int):
-        self.width = width
+        if width % 2 == 1:
+            self.width = width
+        else:
+            self.width = width + 1
         self.tiles = {}
 
     def create_board(self):
         for i in range(self.width):
             for j in range(self.width):
-                # topleft corner
+                # top left corner
                 if i==0 and j==0:
-                    self.tiles.update({(j,i) : Tile(2, 1, 0)})
-                # topright corner
+                    self.tiles.update({(j,i) : Tile(TileType.CORNER, TileOrientation.EAST, TreasureType.YELLOW)})
+                # top right corner
                 if i == 0 and j == self.width-1:
-                    self.tiles.update({(j, i): Tile(2, 2, 1)})
-                # bottomeft corner
+                    self.tiles.update({(j, i): Tile(TileType.CORNER, TileOrientation.SOUTH, TreasureType.BLUE)})
+                # bottom left corner
                 if i == self.width-1 and j == 0:
-                    self.tiles.update({(j, i): Tile(2, 0, 2)})
-                # bottomeft corner
+                    self.tiles.update({(j, i): Tile(TileType.CORNER, TileOrientation.NORTH, TreasureType.RED)})
+                # bottom right corner
                 if i == self.width-1 and j == self.width-1:
-                    self.tiles.update({(j, i): Tile(2, 3, 3)})
+                    self.tiles.update({(j, i): Tile(TileType.CORNER, TileOrientation.WEST, TreasureType.GREEN)})
 
                 # top row
                 if i == 0 and j%2==1 and 0<j>self.width-1:
-                    self.tiles.update({(j,i) : Tile(1, 2)})
+                    self.tiles.update({(j,i) : Tile(TileType.T_PIECE, TileOrientation.EAST)})
                 # bottom row
                 elif i == self.width-1 and j%2==1 and 0<j>self.width-1:
-                    self.tiles.update({(j, i): Tile(1, 0)})
+                    self.tiles.update({(j, i): Tile(TileType.T_PIECE, TileOrientation.NORTH)})
                 # left row
                 elif j == 0 and i%2==1 and 0<i>self.width-1:
-                    self.tiles.update({(j,i) : Tile(1,1)})
+                    self.tiles.update({(j,i) : Tile(TileType.T_PIECE,TileOrientation.EAST)})
                 # right row
                 elif j == self.width-1 and i%2==1 and 0<i>self.width-1:
-                    self.tiles.update({(j, i): Tile(1, 3)})
+                    self.tiles.update({(j, i): Tile(TileType.T_PIECE, TileOrientation.WEST)})
                 # middle
                 elif i%2==1 and j%2==1:
-                    self.tiles.update({(j,i) : Tile(1, randint(0,3))})
+                    self.tiles.update({(j,i) : Tile(TileType.T_PIECE, TileOrientation(randint(0,3)))})
                 # rest
                 else:
                     self.tiles.update({(j,i) : None})
 
     def fill_board(self):
-        stack = [Tile(2, randint(0,3)) for i in range(14)]
-        stack += [Tile(1, randint(0,3)) for i in range(5)]
-        stack += [Tile(0, randint(0,1)) for i in range(12)]
+        stack = [Tile(TileType.CORNER, TileOrientation(randint(0,3))) for i in range(14)]
+        stack += [Tile(TileType.T_PIECE, TileOrientation(randint(0,3))) for i in range(5)]
+        stack += [Tile(TileType.STRAIGHT, TileOrientation(randint(0,1))) for i in range(12)]
         stack.shuffle()
 
         for i in range(self.width):
             for j in range(self.width):
-                if self.tiles[(j,i)] == None:
+                if self.tiles[(j,i)] is None:
                     self.tiles.update({(j,i) : stack[0]})
                     stack = stack[1:]
 
