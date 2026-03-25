0e83ae8b69323e32df7470296097820ca9f5c2bf
Tamay <tengin@students.uni-mainz.de>
2026-03-25 09:39:40 +0100

    add treasure model

--- server/models/treasure.py
+++ server/models/treasure.py
@@ -1,10 +1,27 @@
 # Author: Tamay Engin
 
 from sqlmodel import Field, SQLModel
+from enum import StrEnum
+
+class TreasureType(StrEnum):
+    BOOK = "BOOK"
+    OWL = "OWL"
+    # TODO: add other Treasures
 
 class Treasure(SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    player_id: int = Field(default=None, foreign_key="user.id")
-    collected: bool
-    order: int
-    type: int
+
+    # The player who needs collect this treasure
+    player_id: uuid.UUID = Field(default=None, foreign_key="player.id", index=True)
+
+    # The type of treasure
+    treasure_type: TreasureType
+
+    # Order of this target card in the players treasure sequence
+    order_index: int = Field(index=True)
+
+    # Whether this treasure has already been collected
+    collected: bool = Field(default=False, index=True)
+
+    # When this treasure was collected
+    collected_at: Optional[datetime] = Field(default=None)
