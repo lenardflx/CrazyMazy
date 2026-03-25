e8ad7db0ea219323127d10a01e6bc9529e268e5c
Raphael <43720275+PXAV@users.noreply.github.com>
2026-03-25 12:56:56 +0100

    db: add player service

--- server/db/service.py
+++ server/db/service.py
@@ -1,11 +1,31 @@
+from typing import Optional
 from uuid import UUID
 
 from server.db import GameRepository
+from server.db.repo import PlayerRepository
+from shared.models import GamePhase, GameData
+
+
+class PlayerService:
+    def __init__(self, player_repo: PlayerRepository):
+        self.player_repo = player_repo
+
 
 
 class GameService:
     def __init__(self, game_repo: GameRepository):
         self.game_repo = game_repo
 
-    def find_joinable_game(self):
-        self.game_repo.find_by_game_id(UUID("a816ab9d-a268-4796-8654-78f4d00ad4fb"))
+    def find_joinable_game(self, join_code: str) -> Optional[GameData]:
+        """
+        Finds a game with the given join code and checks whether it currently
+        accepts
+
+        :param join_code:
+        :return:
+        """
+        game = self.game_repo.find_by_join_code(join_code)
+        if game is not None and game.game_phase == GamePhase.PREGAME:
+            return game
+        return None
+
