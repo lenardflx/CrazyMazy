9f68e66e357ee6f872add5c2057011f84d14652e
lenardflx <lenard.felix@outlook.com>
2026-03-25 13:50:52 +0100

    move service.py out of db folder

--- server/db/__init__.py
+++ server/db/__init__.py
@@ -2,7 +2,7 @@ from server.db.memory_repo import (
     GameRepositoryInMemory,
     PlayerRepositoryInMemory,
 )
-from server.db.service import GameService
+from server.service import GameService
 
 game_repository = GameRepositoryInMemory()
 player_repository = PlayerRepositoryInMemory()
