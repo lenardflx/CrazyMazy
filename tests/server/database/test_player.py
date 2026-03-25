775e44e9de6efebcc21f4f72e7ce5edd407585c8
Tamay <tengin@students.uni-mainz.de>
2026-03-25 15:50:45 +0100

    db: model tests

--- tests/server/database/test_player.py
+++ tests/server/database/test_player.py
@@ -1,25 +1,31 @@
 # Author: Tamay Engin
 
-from server.db.sql_repo.py import PlayerRepositorySQL
+from server.db.sql_repo import PlayerRepositorySQL
+from shared.models import PlayerColor
+from uuid import UUID, uuid4
 
 def test_create_player():
-    player = PlayerRepositorySQL.create_player("Name")
-    results = PlayerRepositorySQL.find_by_id(player.id)
+    player_repository_sql = PlayerRepositorySQL()
+    player = player_repository_sql.create_player("Name", uuid4(), uuid4(), 0, PlayerColor.BLUE, uuid4())
+    results = player_repository_sql.find_by_id(player.id)
     assert player == results[0]
 
 def test_find_player_by_id():
-    player = PlayerRepositorySQL.create_player("Name")
-    results = PlayerRepositorySQL.find_by_id(player.id)
+    player_repository_sql = PlayerRepositorySQL()
+    player = player_repository_sql.create_player("Name", uuid4(), uuid4(), 0, PlayerColor.BLUE, uuid4())
+    results = player_repository_sql.find_by_id(player.id)
     assert player == results[0]
 
 def test_update_player():
-    player = PlayerRepositorySQL.create_player("Name")
+    player_repository_sql = PlayerRepositorySQL()
+    player = player_repository_sql.create_player("Name", uuid4(), uuid4(), 0, PlayerColor.BLUE, uuid4())
     player.display_name = "New_Name"
-    player = PlayerRepositorySQL.update_player(player)
-    results = PlayerRepositorySQL.find_by_id(player.id)
+    player = player_repository_sql.update_player(player)
+    results = player_repository_sql.find_by_id(player.id)
     assert results[0].display_name == player.display_name
 
 def test_player():
     test_create_player()
     test_find_player_by_id()
     test_update_player()
+    print("tests for player model successful")
--- tests/server/database/test_treasure.py
+++ tests/server/database/test_treasure.py
@@ -1,35 +1,51 @@
 # Author: Tamay Engin
 
-from server.db.sql_repo.py import TreasureRepositorySQL
+from server.db.sql_repo import TreasureRepositorySQL
+from shared.models import TreasureType, TreasureData
+from uuid import UUID, uuid4
 
-# treasure_data = 
+def create_treasure_data():
+    treasure_data = TreasureData()
+    treasure_data.id = uuid4()
+    treasure_data.treasure_type = TreasureType.BOOK
+    treasure_data.order_index = 0
+    treasure_data.collected = 0
+    treasure_data.player_id = uuid4()
+    treasure_data.order_index: int = Field(index=True)
+    treasure_data.collected = False
+    return treasure_data
 
 def test_create_treasure():
-    treasure = TreasureRepositorySQL.create_treasure(treasure_data)
-    results = TreasureRepositorySQL.find_by_id(treasure.id)
+    treasure_repository_sql = TreasureRepositorySQL()
+    treasure = treasure_repository_sql.create_treasure(create_treasure_data())
+    results = treasure_repository_sql.find_by_id(treasure.id)
     assert results[0] == treasure
 
 def test_find_by_id():
-    treasure = TreasureRepositorySQL.create_treasure(treasure_data)
-    results = TreasureRepositorySQL.find_by_id(treasure.id)
+    treasure_repository_sql = TreasureRepositorySQL()
+    treasure = treasure_repository_sql.create_treasure(create_treasure_data())
+    results = treasure_repository_sql.find_by_id(treasure.id)
     assert results[0] == treasure
 
 def test_list_by_player_id():
-    treasure = TreasureRepositorySQL.create_treasure(treasure_data) 
-    results = TreasureRepositorySQL.list_by_player_id(treasure.player_id)
+    treasure_repository_sql = TreasureRepositorySQL()
+    treasure = treasure_repository_sql.create_treasure(create_treasure_data()) 
+    results = treasure_repository_sql.list_by_player_id(treasure.player_id)
     assert results[0] == treasure
 
 def test_update_treasure():
-    treasure = TreasureRepositorySQL.create_treasure(treasure_data)
+    treasure_repository_sql = TreasureRepositorySQL()
+    treasure = treasure_repository_sql.create_treasure(create_treasure_data())
     treasure.order_index = 0
-    TreasureRepositorySQL.update_treasure(treasure)
-    results = TreasureRepositorySQL.find_by_id(treasure.id)
+    treasure_repository_sql.update_treasure(treasure)
+    results = treasure_repository_sql.find_by_id(treasure.id)
     assert results[0] == treasure
 
 def test_delete_treasure():
-    treasure = TreasureRepositorySQL.create_treasure(treasure_data)
-    TreasureRepositorySQL.delete_treasure(treasure.id)
-    results = TreasureRepositorySQL.find_by_id(treasure.id)
+    treasure_repository_sql = TreasureRepositorySQL()
+    treasure = treasure_repository_sql.create_treasure(create_treasure_data())
+    treasure_repository_sql.delete_treasure(treasure.id)
+    results = treasure_repository_sql.find_by_id(treasure.id)
     assert len(results) < 1
 
 def test_treasure():
@@ -37,3 +53,4 @@ def test_treasure():
     test_list_by_player_id()
     test_update_treasure()
     test_delete_treasure()
+    print("tests for treasure model successful")
