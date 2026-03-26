679062b12f5469ca0e9a7a919c1145893dfe7ef8
Tamay <tengin@students.uni-mainz.de>
2026-03-26 11:16:05 +0100

    Remove test file

--- tests/server/database/tests.py
+++
@@ -1,9 +0,0 @@
-# Author: Tamay Engin, Lukas Saur-Brosch
-
-from tests.server.database.test_player import test_player
-from tests.server.database.test_game import test_game
-
-
-def run_tests():
-    test_player()
-    test_game()
