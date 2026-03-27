46ad3758c491aae715426ddd522a2110bdb769d4
CIonescu-jgu <cionescu@uni-mainz.de>
2026-03-27 10:55:29 +0100

    Apply Button
    Ein Apply Button in den Settings, der die Einstellungen übernimmt.

--- client/screens/menu/settings_screen.py
+++ client/screens/menu/settings_screen.py
@@ -6,7 +6,7 @@ from typing import TYPE_CHECKING
 
 import pygame as pg
 
-from client.ui.controls import Checkbox, Slider
+from client.ui.controls import Checkbox, Slider, Button
 from client.ui.theme import TEXT_PRIMARY
 from client.screens.menu.menu_screen import MenuScreen
 
@@ -38,7 +38,12 @@ class SettingsScreen(MenuScreen):
             Slider(pg.Rect(0, 166, control_width, 12), "Music Volume", settings.music_volume),
             Slider(pg.Rect(0, 240, control_width, 12), "Effects Volume", settings.effects_volume),
         ]
-        self.fullscreen_checkbox = Checkbox(pg.Rect(0, 364, control_width, 32), "Fullscreen", settings.fullscreen)
+        self.fullscreen_checkbox = Checkbox(pg.Rect(0, 320, 128, 32), "Fullscreen", settings.fullscreen)
+        self.apply_button = Button(
+                pg.Rect(720, 500, 120, 46),
+                "Apply",
+                lambda: self._sync_settings(),
+)
 
     def _sync_settings(self) -> None:
         settings = self.scene_manager.client_settings
@@ -46,14 +51,10 @@ class SettingsScreen(MenuScreen):
         settings.set_music_volume(self.volume_sliders[1].value)
         settings.set_effects_volume(self.volume_sliders[2].value)
         settings.set_fullscreen(self.fullscreen_checkbox.value)
-        #settings.master_volume = self.volume_sliders[0].value
-        #settings.music_volume = self.volume_sliders[1].value
-        #settings.effects_volume = self.volume_sliders[2].value
-        #settings.fullscreen = self.fullscreen_checkbox.value
 
     def _apply_layout(self) -> None:
         left = self.content_area.x
-        controls: list[Slider | Checkbox] = [*self.volume_sliders, self.fullscreen_checkbox]
+        controls: list[Slider | Checkbox | Button] = [*self.volume_sliders, self.fullscreen_checkbox, self.apply_button]
         for control, y in zip(controls, self.CONTROL_LAYOUT, strict=False):
             control.rect.x = left
             control.rect.y = self.content_area.y + y
@@ -68,8 +69,9 @@ class SettingsScreen(MenuScreen):
         if pointer_in_content:
             changed = self.fullscreen_checkbox.handle_event(event) or changed
 
+        self.apply_button.handle_event(event)
         if changed:
-            self._sync_settings()
+            pass
 
     def draw_content(self, rect: pg.Rect) -> None:
         super().draw_content(rect)
@@ -79,6 +81,7 @@ class SettingsScreen(MenuScreen):
         for slider in self.volume_sliders:
             slider.draw(self.surface, self.body_font, self.small_font)
         self.fullscreen_checkbox.draw(self.surface, self.body_font)
+        self.apply_button.draw(self.surface, self.button_font)
 
     def _draw_section_header(self, title: str, y: int) -> None:
         header_y = self.content_area.y + y
