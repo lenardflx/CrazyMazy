b00b2fce1a543a92a35f7634aedbc4bc184d6c7f
Tamay <tengin@students.uni-mainz.de>
2026-03-30 11:22:08 +0200

    Kommentare hinzufügen

--- tests/server/database/test_player.py
+++ tests/server/database/test_player.py
@@ -5,21 +5,16 @@ from server.db.engine import create_engine_for_environment
 from shared.types.enums import PlayerColor
 from uuid import uuid4
 
-def test_create_player():
+def test_create_player_and_find_by_id():
+    """Create an instance of the table Player and raise an AssertionError if the row is not found"""
     player_repository_sql = PlayerRepositorySQL(create_engine_for_environment("test"))
     player = player_repository_sql.create_player("Name", f"{uuid4()}", uuid4(), 0, PlayerColor.BLUE)
     result = player_repository_sql.find_by_id(player.id)
     assert result is not None
     assert player.id == result.id
 
-def test_find_player_by_id():
-    player_repository_sql = PlayerRepositorySQL(create_engine_for_environment("test"))
-    player = player_repository_sql.create_player("Name",f"{uuid4()}", uuid4(), 0, PlayerColor.BLUE)
-    result = player_repository_sql.find_by_id(player.id)
-    assert result is not None
-    assert player.id == result.id
-
 def test_update_player():
+    """Create and update an instance of the table Player and raise an AssertionError if the row was not updated in the table"""
     player_repository_sql = PlayerRepositorySQL(create_engine_for_environment("test"))
     player = player_repository_sql.create_player("Name", f"{uuid4()}", uuid4(), 0, PlayerColor.BLUE)
     player.display_name = "New_Name"
--- tests/server/database/test_treasure.py
+++ tests/server/database/test_treasure.py
@@ -13,14 +13,8 @@ def create_treasure_data():
         order_index=0
     )
 
-def test_create_treasure():
-    treasure_repository_sql = TreasureRepositorySQL(create_engine_for_environment("test"))
-    treasure = treasure_repository_sql.create_treasure(create_treasure_data())
-    result = treasure_repository_sql.find_by_id(treasure.id)
-    assert result is not None
-    assert result.id == treasure.id
-
-def test_find_by_id():
+def test_create_treasure_and_find_by_id():
+    """Create an instance of the table Treasure and raise an AssertionError if the row is not found in the table"""
     treasure_repository_sql = TreasureRepositorySQL(create_engine_for_environment("test"))
     treasure = treasure_repository_sql.create_treasure(create_treasure_data())
     result = treasure_repository_sql.find_by_id(treasure.id)
@@ -28,6 +22,7 @@ def test_find_by_id():
     assert result.id == treasure.id
 
 def test_list_by_player_id():
+    """Create an instance of the table Treasure and raise an AssertionError if the row is not found using the player_id"""
     treasure_repository_sql = TreasureRepositorySQL(create_engine_for_environment("test"))
     treasure = treasure_repository_sql.create_treasure(create_treasure_data()) 
     results = treasure_repository_sql.list_by_player_id(treasure.player_id)
@@ -35,6 +30,7 @@ def test_list_by_player_id():
     assert results[0].id == treasure.id
 
 def test_update_treasure():
+    """Create and update an instance of the table Treasure and raise an AssertionError if the row was not updated in the table"""
     treasure_repository_sql = TreasureRepositorySQL(create_engine_for_environment("test"))
     treasure = treasure_repository_sql.create_treasure(create_treasure_data())
     treasure.order_index = 1
@@ -44,6 +40,7 @@ def test_update_treasure():
     assert result.order_index == 1
 
 def test_delete_treasure():
+    """Create and delete an instance of the table Treasure and raise an AssertionError if the row is found in the table"""
     treasure_repository_sql = TreasureRepositorySQL(create_engine_for_environment("test"))
     treasure = treasure_repository_sql.create_treasure(create_treasure_data())
     treasure_repository_sql.delete_treasure(treasure.id)
