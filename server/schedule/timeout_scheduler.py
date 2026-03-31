571e4319d8fd745859637fa0165b6eae931d8413
Raphael <43720275+PXAV@users.noreply.github.com>
2026-03-31 12:10:40 +0200

    add: properly implement timeout scheduler

--- server/schedule/timeout_scheduler.py
+++ server/schedule/timeout_scheduler.py
@@ -1,5 +1,11 @@
+import time
+
 from server.db.runtime import game_service
+from server.handlers._responses import snapshot_response
+from server.network.outgoing import flush_outgoing
 from shared.async_scheduler import AsyncScheduler
+from shared.protocol import ErrorCode
+from shared.types.enums import TurnPhase
 
 
 class TimeoutScheduler(AsyncScheduler):
@@ -9,5 +15,18 @@ class TimeoutScheduler(AsyncScheduler):
     def tick(self):
         active_games = game_service.find_active_games()
         for game in active_games:
+            now = time.time_ns() // 1_000_000
+            if now < game.turn_end_timestamp:
+                continue
 
-            game.turn_start_timestamp
+            if game.turn_phase == TurnPhase.SHIFT:
+                state = game_service.give_up(game.current_player_id)
+                if isinstance(state, ErrorCode):
+                    continue # todo handle error here
+            else:
+                state = game_service.end_turn(game.current_player_id)
+                if isinstance(state, ErrorCode):
+                    continue # todo handle error here
+
+            outgoing_messages = snapshot_response(state)
+            flush_outgoing(outgoing_messages)
