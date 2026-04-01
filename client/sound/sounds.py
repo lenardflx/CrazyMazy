04f61cb89e5773fbe0e24111af483aae7ce0a924
Lukas Saur-Brosch <lsaurbro@students.uni-mainz.de>
2026-04-01 11:56:20 +0200

    sounds geadded und vorläufiger soundtrack

--- client/sound/sounds.py
+++ client/sound/sounds.py
@@ -7,12 +7,12 @@ MUSIC_TRACKS: dict[str, str] = {
 SFX: dict[str, str] = {
     "button_click": "assets/sounds/button.wav",
     "tile_shift": "assets/sounds/tile_shift.wav",
-    "player_move": "assets/sounds/player_move.wav",
+    "player_move": "assets/sounds/move_player.wav",
     "win": "assets/sounds/button.wav", # add
     "lose": "assets/sounds/button.wav", # add
     "your_turn": "assets/sounds/button.wav", # add
-    "timer_beep": "assets/sounds/button.wav", # add
+    "timer_beep": "assets/sounds/timer.wav", # add
     "treasure_collect": "assets/unused/place_player_1.wav", # add
     "treasure_collect_meow": "assets/sounds/meow.mp3",
-    "error": "assets/sounds/button.wav", # add
+    "error": "assets/sounds/error_sound.wav", # add
 }
